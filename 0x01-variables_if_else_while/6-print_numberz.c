89f2292071baa06691fc023c37776d4c8a0e5669
Andres <lopezgand@gmail.com>
2019-02-07 20:17:43 +0000

    printing a int whit char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c <= 9; c++)
+		putchar('0' + c);
+	putchar('\n');
+	return (0);
+}

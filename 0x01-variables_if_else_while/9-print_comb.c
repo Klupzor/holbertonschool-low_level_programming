2ac92501937243f3e29305279f5293ea37a63eb4
Andres <lopezgand@gmail.com>
2019-02-07 22:01:32 +0000

    separate numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 48 ; c <= 57; c++)
+	{
+		putchar(c);
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}

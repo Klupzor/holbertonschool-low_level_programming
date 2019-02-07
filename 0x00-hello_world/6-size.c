1d483d7caf5dcdf6460a02b3f6d5a92a03d29c77
Andres <lopezgand@gmail.com>
2019-02-07 00:31:55 +0000

    fixing styling

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,14 +9,14 @@ int main(void)
 {
 int integerType;
 long int longIntType;
-long long int longLongIntType; 
+long long int longLongIntType;
 float floatType;
 char charType;
 
-printf("Size of a char: %ld byte(s)\n",sizeof(charType));
-printf("Size of an int: %ld byte(s)\n",sizeof(integerType));
-printf("Size of a long int: %ld byte(s)\n",sizeof(longIntType));
-printf("Size of a long long int: %ld byte(s)\n",sizeof(longLongIntType));
-printf("Size of a float: %ld byte(s)",sizeof(floatType));
+printf("Size of a char: %d byte(s)\n", sizeof(charType));
+printf("Size of an int: %d byte(s)\n", sizeof(integerType));
+printf("Size of a long int: %d byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %d byte(s)\n", sizeof(longLongIntType));
+printf("Size of a float: %d byte(s)", sizeof(floatType));
 return (0);
 }

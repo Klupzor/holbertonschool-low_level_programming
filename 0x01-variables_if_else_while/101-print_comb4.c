bb62ddf7f6b816c0aa92233a0f589758e0a21dee
Andres <lopezgand@gmail.com>
2019-02-08 00:16:00 +0000

    print 3 diferents combinations of numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+	int b;
+	int a;
+
+	for (b = 48 ; b <= 57; b++)
+	{
+	for (c = b + 1; c <= 57; c++)
+	{
+	for (a = c + 1; a <= 57; a++)
+	{	
+		putchar(b);
+		putchar(c);
+		putchar(a);
+		if (c + b + a < 168)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}

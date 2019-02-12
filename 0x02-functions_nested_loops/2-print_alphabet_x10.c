f1d32baeac85d5760e2ce683eb24526bda3ab975
Andres <lopezgand@gmail.com>
2019-02-12 15:22:50 +0000

    printing 10 times the alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - writes 10 times the alphabet in lowercase
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_alphabet_x10(void)
+{
+	char a;
+	int c;
+
+	for (c = 0 ; c <= 10 ; c++)
+	{
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+	}
+}

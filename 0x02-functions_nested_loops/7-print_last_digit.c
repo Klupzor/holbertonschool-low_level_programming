f4518ba4366b93e76fb9aa82a1c94dab21c70961
Andres <lopezgand@gmail.com>
2019-02-12 19:54:55 +0000

    printing the last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+# include "holberton.h"
+/**
+ * print_last_digit - print last number
+ *@c: Character input
+ * Return: last number
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int print_last_digit(int c)
+{
+	int r;
+
+	if (c < 0)
+	{
+		c = -c;
+	}
+	r = (c % 10);
+	_putchar(r + '0');
+	return (r);
+}

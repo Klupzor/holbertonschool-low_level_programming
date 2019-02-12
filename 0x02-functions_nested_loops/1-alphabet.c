eea483822fd4568d8d0d4f194eba2445040127dc
Andres <lopezgand@gmail.com>
2019-02-12 14:10:45 +0000

    printing alphabet with a function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+
+		_putchar(a);
+
+	}
+	_putchar('\n');
+	return (0);
+}

ff421c9d889b9d8f1cad06072b6d07d619b68429
Andres <lopezgand@gmail.com>
2019-02-12 14:01:58 +0000

    writing a function to print a char

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "holberton.h"
 /* more headers goes there */
 
 /**
@@ -8,6 +8,15 @@
  */
 int main(void)
 {
-	puts("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 	return (0);
 }

8a033eb2bdca905c0da83bf110073ee31dd48ffb
Andres <lopezgand@gmail.com>
2019-02-12 22:10:20 +0000

    adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+/**
+ * add - adds two integers
+ * @a: Character input
+ * @b: Character input
+ * Return: adds
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int a, int b)
+{
+	int r;
+
+	r = a + b;
+	return (r);
+}

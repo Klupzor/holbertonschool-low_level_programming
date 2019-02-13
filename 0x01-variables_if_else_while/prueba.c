d8a21932a6691468194b054454058e6de3804195
Andres <lopezgand@gmail.com>
2019-02-13 01:40:44 +0000

    fixing

---
+++ 0x01-variables_if_else_while/prueba.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}

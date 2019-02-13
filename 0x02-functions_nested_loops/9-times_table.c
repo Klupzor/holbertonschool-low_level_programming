ca6782290d68d559662754e38e0e9e8eba1064ee
Andres <lopezgand@gmail.com>
2019-02-13 02:13:23 +0000

    fixing a number

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,20 +15,23 @@ void times_table(void)
 		for (h = 0 ; h <= 9 ; h++)
 		{
 			r = H * h;
-			if (r > 10)
+			if (r >= 10)
+			{
 				_putchar(r / 10 + '0');
+				_putchar(r % 10 + '0');
+			}
 			else
-			  {
+			{
 				if (r != 0)
 					_putchar(' ');
-			  }
-			_putchar(r % 10 + '0');
+				_putchar(r + '0');
+			}
 			if (h < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
 				if (H == 0)
-					_putchar(' ');
+				_putchar(' ');
 			}
 		}
 		_putchar(10);

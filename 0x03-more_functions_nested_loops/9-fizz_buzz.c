13b47b175d4de25c7c69c9e8de04f607ca548812
Andres <lopezgand@gmail.com>
2019-02-14 21:14:09 +0000

    printing fizz buzz

---
+++ 0x03-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - printing fizz buzz.
+ *
+ * Return: On success result.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void main(void)
+{
+	int c;
+
+	for (c = 1 ; c <= 100 ; c++)
+	{
+		if (c % 3 == 0)
+			printf("Fizz");
+		if (c % 5 == 0)
+			printf("Buzz");
+		if (c % 3 != 0 && c % 5 != 0)
+			printf("%i", c);
+		printf(" ");
+	}
+	printf("\n");
+}

19bbe71e5f6293bb5f9ed515eef53fab7701b1cc
Andres <lopezgand@gmail.com>
2019-02-14 15:42:06 +0000

    creating file to evaluate if the character is upercase

---
+++ 0x03-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _isupper(char c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}

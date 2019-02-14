94f54b98b6f6598077b18f0ab2d28d529cea1aca
Andres <lopezgand@gmail.com>
2019-02-14 20:33:46 +0000

    printing diagonal

---
+++ 0x03-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+/**
+ * print_giagonal - print diagonal line whit space n
+ *
+ *@n: Numbers of lines.
+ * Return: On success result.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_diagonal(int n)
+{
+	int c, d;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (d = 0 ; d < n ; d++)
+		{
+			for (c = 0 ; c < d ; c++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}

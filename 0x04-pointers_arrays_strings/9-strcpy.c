99bb3f0898c442f2d39e339a1f75ddf3bba79035
klupzor <lopezgand@gmail.com>
2019-02-20 02:16:56 +0000

    copy string

---
+++ 0x04-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strcpy - prints reverse  copy.
+ *
+ * @s: char string.
+ * @a: copy char string.
+ * Return: Always 0.
+ */
+char *_strcpy(char *s, char *a)
+{
+	int c, i;
+
+	for (i = 0 ; a[i] != '\0' ; i++)
+	{
+	}
+	for (c = 0 ; c < i &&  a[c] != '\0' ; c++)
+		s[c] = a[c];
+	for ( ; c < i ; c++)
+		s[c] = '\0';
+	return (s);
+}

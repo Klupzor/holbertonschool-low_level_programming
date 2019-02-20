58fb8ac75a84616d86cbd80db9bbe40da177e272
klupzor <lopezgand@gmail.com>
2019-02-20 00:08:43 +0000

    print in reverse

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string, in reverse.
+ *
+ * @s: char string.
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int i, c;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	for (c = i ; c >= 0; c--)
+	{
+		_putchar(s[c]);
+	}
+	_putchar('\n');
+}

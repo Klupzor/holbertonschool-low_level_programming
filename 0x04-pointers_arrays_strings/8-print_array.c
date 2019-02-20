28fa2355f2b770b37d32f78118c7f2346c7d665e
klupzor <lopezgand@gmail.com>
2019-02-20 01:29:57 +0000

    printing n characters

---
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_array - display a string.
+ *
+ * @s: ints.
+ * @n: number of ints.
+ * Return: Always 0.
+ */
+void print_array(int *s, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%i", s[i]);
+		if (i < n - 1)
+			printf(" ,");
+	}
+	printf("\n");
+}

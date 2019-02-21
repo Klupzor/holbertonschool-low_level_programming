ded7490ec77273f3e7df6c8cad5cdf189fc0c17a
klupzor <lopezgand@gmail.com>
2019-02-21 20:45:31 +0000

    capitalizes all words of a string.

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * cap_string -Separators of words: space, tabulation, new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *
+ * @s: char to capitalize.
+ * Return: Converted.
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		if (s[i] == ',' || s[i] == ';' || s[i] == '.' || s[i] == '!' || s[i] == '?' || 
+		s[i] == '"' || s[i] == '(' || s[i] == ')' || s[i] == '{' || s[i] == '}' || s[i] == 9 || s[i] == ' '|| s[i] == '\n')
+			if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				s[i + 1] -= 32;
+	}
+	return (s);
+}

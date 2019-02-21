bd1f87d82ae9f82f7fb665bea13198cbf6bbdc7e
klupzor <lopezgand@gmail.com>
2019-02-21 15:36:20 +0000

    concat 2 char on n bytes

---
+++ 0x05-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+/**
+ * _strncat - prints concat string whit n bytes.
+ *
+ * @dest: char dest.
+ * @src: copy char string.
+ * @n: bytes.
+ * Return: char.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int c, i, cont = 0;
+
+	n *= 8;
+	for (i = 0 ; dest[i] != '\0' ; i++)
+	{
+	}
+	for (c = i ; src[cont] != '\0'; c++)
+	{
+		if (c < n - 1)
+		{
+			dest[c] = src[cont];
+			cont++;
+		}
+		else
+			break;
+	}
+	dest[c] = '\0';
+	return (dest);
+}

6fb2e9112d5c1d0e6675f2988f6022eb8e80ccef
klupzor <lopezgand@gmail.com>
2019-02-21 19:46:32 +0000

    Reverse int array

---
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * reverse_array - prints reverse  int array.
+ *
+ * @i: int array;
+ */
+void reverse_array(int *s, int n)
+{
+	int i, c, p = 0;
+
+	for (c = n - 1 ; c >= n / 2 ; c--)
+	{
+		i = s[p];
+		s[p] = s[c];
+		s[c] = i;
+		p++;
+	}
+}

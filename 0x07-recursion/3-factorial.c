f333a9d0da56c5356c14f2b763d15f535c962fe2
klupzor <lopezgand@gmail.com>
2019-02-27 19:55:09 +0000

    factorial

---
+++ 0x07-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * factorial - factorial.
+ *
+ *@n: number to be factorial.
+ * Return: factorial number.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else 
+	if (n <= 1)
+		return (1);
+	return (n * factorial(n - 1));
+}

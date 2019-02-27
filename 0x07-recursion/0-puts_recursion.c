d57749b30a879443b5dc68db695fe6263f5d3048
klupzor <lopezgand@gmail.com>
2019-02-27 16:27:55 +0000

    printing string

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
+	}
+	else
+		_putchar('\n');
+}

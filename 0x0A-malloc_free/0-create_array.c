5071a2e3862ad80da2d9209f4cf108ca9e36c813
klupzor <lopezgand@gmail.com>
2019-03-05 19:00:16 +0000

    using malloc to create a dynamic array

---
+++ 0x0A-malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int i;
+
+	if (size == 0)
+		return (0);
+	array = malloc(size * sizeof(char));
+	for (i = 0 ; i < size ; i++)
+	{
+		array[i] = c;
+	}
+	return(array);
+}

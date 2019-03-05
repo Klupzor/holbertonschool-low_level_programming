57659a2354aa0cea7fe5547f8da28d47f6a16bf0
klupzor <lopezgand@gmail.com>
2019-03-05 22:51:26 +0000

    free memory

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * clean - free memory
+ * @array: pointer to be liberate
+ * @i: size of matrix.
+ *
+ */
+
+void clean(int **array, int i)
+{
+	for (; i >= 0 ; i--)
+	{
+		free(array[i]);
+	}
+	free(array);
+}
+
+/**
  * alloc_grid - create array row x col
  * @col: string to copy
  * @row: string to copy
@@ -17,14 +33,19 @@ int **alloc_grid(int col, int row)
 		return (NULL);
 	array = (int **) malloc(row * sizeof(int));
 	if (array == NULL)
-		return (NULL);
-	for (i = 0 ; i < row ; i++)
 	{
-		array[i] = (int *) malloc(col * sizeof(int));
+		free(array);
+		return (NULL);
 	}
 
 	for (i = 0 ; i < row ; i++)
 	{
+		array[i] = (int *) malloc(col * sizeof(int));
+		if (array[i] == NULL)
+		{
+			clean(array, i);
+			return (NULL);
+		}
 		for (j = 0 ; j < col ; j++)
 		{
 			array[i][j] = 0;

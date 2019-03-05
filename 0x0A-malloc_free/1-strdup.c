5ef136b302125b169a12facc7b72c5bfbf470f98
klupzor <lopezgand@gmail.com>
2019-03-05 19:45:07 +0000

    copy 2 strings

---
+++ 0x0A-malloc_free/1-strdup.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - create a dynamic copy array.
+ * @str: string to copy
+ *
+ * Return: ponter array or null.
+ */
+
+char *_strdup(char *str)
+{
+	char *array;
+	unsigned int i;
+
+	array = malloc(sizeof(str));
+	for (i = 0 ; str[i] != '\0' ; i++)
+	{
+		array[i] = str[i];
+	}
+	if (array != 0)
+		return (array);
+	return (0);
+}

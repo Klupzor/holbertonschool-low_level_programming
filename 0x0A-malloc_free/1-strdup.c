ac3f96cf7ee69bd03d76a306f6d14649de3bdc27
klupzor <lopezgand@gmail.com>
2019-03-05 20:12:32 +0000

    fixing size

--- 0x0A-malloc_free/1-strdup.c
+++ 0x0A-malloc_free/1-strdup.c
@@ -15,7 +15,7 @@ char *_strdup(char *str)
 
 	if (!str)
 		return (0);
-	array = malloc(sizeof(str) + 1);
+	array = malloc(sizeof(str) * sizeof(char));
 	for (i = 0 ; str[i] != '\0' ; i++)
 	{
 		array[i] = str[i];

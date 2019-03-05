4f0da935a00ee7b72c40ba2d472bad2cbb55ca37
klupzor <lopezgand@gmail.com>
2019-03-05 21:17:15 +0000

    concatenate 2 strings

---
+++ 0x0A-malloc_free/2-str_concat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * str_concat - concatenate 2 strings
+ * @s1: string to copy
+ * @s2: string to copy
+ *
+ * Return: ponter array or null.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *array;
+	int i, j, size, size2;
+
+	if (!s1 || !s2)
+		return (0);
+	for (size = 1 ; s1[size] != '\0' ; size++)
+	{
+	}
+	for (size2 = 1 ; s2[size2] != '\0' ; size2++)
+	{
+	}
+	array =  malloc(((size + 1) + (size2 + 1)) * sizeof(char));
+	if (array == NULL)
+		return (NULL);
+	for (i = 0 ; s1[i] != '\0' ; i++)
+	{
+		array[i] = s1[i];
+	}
+	for (j = 0 ; s2[j] != '\0' ; j++)
+	{
+		array[i] = s2[j];
+		i++;
+	}
+
+	array[i] = '\0';
+	return (array);
+}

3f67fb653ef6cf37de43814b34143453120adcfe
klupzor <lopezgand@gmail.com>
2019-03-07 18:39:35 +0000

    fixing styles

--- 0x0B-more_malloc_free/0-malloc_checked.c
+++ 0x0B-more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * malloc_checked - return empty pointer.
+ *
+ *@b: size of pointer
+ * Return: pointer.
+ */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
+
 	p = malloc(b);
 	if (p == NULL)
 		exit(98);
-	return(p);
+	return (p);
 }

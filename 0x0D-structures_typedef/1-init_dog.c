7519f31d8b645dd107398eaab9f9d8903fe736b0
klupzor <lopezgand@gmail.com>
2019-03-11 21:06:30 +0000

    gcc -Wall -pedantic -Werror -Wextra 1-main.c 1-init_dog.c

---
+++ 0x0D-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - init dog struct.
+ *
+ * @d: struct dog.
+ * @name: name dog.
+ * @age: age dog.
+ * @owner: owner dog.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}

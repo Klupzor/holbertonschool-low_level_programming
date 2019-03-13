173c51f85094176343285bacfe6731756dacaefc
klupzor <lopezgand@gmail.com>
2019-03-13 16:13:12 +0000

    printing array

---
+++ 0x0E-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * array_iterator - prints an array.
+ * @array: arrat to print
+ * @size: size of array.
+ * @action: fuction that prints.
+ *
+ * Return: Nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		action(array[i]);
+	}
+}

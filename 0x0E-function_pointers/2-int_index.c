277cc43b59a81a34b3cbc540cb4100175c915cbe
klupzor <lopezgand@gmail.com>
2019-03-13 18:49:49 +0000

    searches for an integer and print the position

---
+++ 0x0E-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * int_index - prints position when its true.
+ * @array: arrat to print
+ * @size: size of array.
+ * @cpm: fuction that compare.
+ *
+ * Return: position.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0 ; i < size ; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}

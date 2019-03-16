ac5b13c35b30fd173d55ba8a62c6de7fa4b007b9
klupzor <lopezgand@gmail.com>
2019-03-16 17:14:46 +0000

    fixing betty in header file

--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,10 +1,11 @@
 #ifndef VARIADIC_FUNCTIONS
 #define VARIADIC_FUNCTIONS
 #include<stdarg.h>
-typedef struct typ {
+typedef struct typ
+{
 	char *c;
 	void (*tp)(va_list var);
-}typ_t;
+} typ_t;
 void print_all(const char * const format, ...);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);

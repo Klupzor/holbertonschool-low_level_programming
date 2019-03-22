4fbc3e6080d0c1dc3876a0a0513be55dba3a7211
klupzor <lopezgand@gmail.com>
2019-03-22 01:30:51 +0000

    Fixing styles

--- 0x11-singly_linked_lists/100-first.c
+++ 0x11-singly_linked_lists/100-first.c
@@ -1,8 +1,13 @@
 #include<stdio.h>
-    /* Apply the constructor attribute to startupfun() so that it is executed before main() */
-    void start(void) __attribute__ ((constructor));
+void start(void) __attribute__ ((constructor));
+
+/**
+ * start - Runs before main
+ *
+ */
 
 void start(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
 }

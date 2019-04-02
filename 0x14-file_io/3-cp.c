955a8863c6ebcfd58875a3366f6f8d31cddb2d7b
klupzor <lopezgand@gmail.com>
2019-04-02 22:23:55 +0000

    copies the content of a file to another file.

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - copies the content of a file to another file.
+ *
+ * @argc: Num of arguments.
+ * @argv: Arguments.
+ * Return: Always 1.
+ */
+
+int main(int argc, char **argv)
+{
+	int fd, rd, ws, cs;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file  %s\n", argv[1]);
+		exit(98);
+	}
+	buffer = malloc(1024);
+	rd = read(fd, buffer, 1024);
+	close(fd);
+	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	ws = write(fd, buffer, rd);
+	if (ws == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to  %s\n", argv[2]);
+		exit(99);
+	}
+	cs = close(fd);
+	if (cs == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", fd);
+		exit(100);
+	}
+	free(buffer);
+	return (1);
+}

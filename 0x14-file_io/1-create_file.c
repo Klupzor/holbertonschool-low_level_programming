6fa2417133a160d1c1c8f72185c9550e8639f221
klupzor <lopezgand@gmail.com>
2019-04-02 21:14:37 +0000

    fixing null case

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -18,14 +18,16 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	for (strsize = 0 ; text_content[strsize] != '\0' ; strsize++)
-	{
-	}
 	rfile = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (rfile == -1)
 		return (-1);
 	if (text_content)
+	{
+		for (strsize = 0 ; text_content[strsize] != '\0' ; strsize++)
+		{
+		}
 		write(rfile, text_content, strsize);
+	}
 
 	close(rfile);
 	return (1);

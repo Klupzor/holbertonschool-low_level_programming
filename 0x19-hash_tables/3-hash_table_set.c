e33fa554ed66d6fc9f840a57cfee43d77715fe59
klupzor <lopezgand@gmail.com>
2019-05-31 19:58:53 +0000

    set to hash table

---
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Set new node to hash table
+ *
+ * @ht: hash tables.
+ * @key: key value.
+ * @value:  Value to save into node
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (strcmp(key, "") == 0 || strcmp(value, "") == 0 || !ht)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	node = (hash_node_t *) malloc(sizeof(hash_node_t));
+	if (!node)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (ht->array[index])
+		node->next = ht->array[index];
+	else
+		node->next = NULL;
+
+	ht->array[index] = node;
+
+	return (1);
+}

5020fdbd02cc723b4f5e9ed3f03d6c1af193ffc6
klupzor <lopezgand@gmail.com>
2019-08-29 00:49:47 +0000

    fixing for

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -13,7 +13,7 @@
 
 int jump_search(int *array, size_t size, int value)
 {
-	size_t range, step, i, prev;
+	size_t range, step, i;
 
 	if (!array)
 		return (-1);
@@ -22,11 +22,10 @@ int jump_search(int *array, size_t size, int value)
 	while (array[step] < value && step <= size - 1)
 	{
 		printf("Value checked array[%lu] = [%i]\n", step, array[step]);
-		prev = step;
 		step += range;
 	}
 	printf("Value found between indexes [%lu] and [%lu]\n", step - range, step);
-	for (i = prev ; i <= (prev + range) && i <= size - 1; i++)
+	for (i = step - range ; i <= (step) && i <= size - 1; i++)
 	{
 		printf("Value checked array[%lu] = [%i]\n", i, array[i]);
 		if (array[i] == value)

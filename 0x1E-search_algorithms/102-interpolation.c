c381bfe2a3e101bf5f75c043fd8d8312f6db5242
klupzor <lopezgand@gmail.com>
2019-08-29 22:11:41 -0500

    fixing style

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -17,6 +17,9 @@ int interpolation_search(int *array, size_t size, int value)
 	low = 0;
 	high = size - 1;
 
+	if (!array)
+		return (-1);
+
 	while (low <= high)
 	{
 		if (low == high)
@@ -25,24 +28,25 @@ int interpolation_search(int *array, size_t size, int value)
 				return (low);
 			return (-1);
 		}
-		
-		index = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+
+		index = low + (((double)(high - low) / (array[high] - array[low]))
+		* (value - array[low]));
 		if (value < array[low] || value > array[high])
 		{
 			printf("Value checked array[%lu] is out of range\n", index);
 			return (-1);
 		}
-		
+
 
 		printf("Value checked array[%lu] = [%i]\n", index, array[index]);
 		if (array[index] == value)
-			return index;
-		
+			return (index);
+
 		if (array[index] < value)
 			low = index + 1;
 		else
 			high = index - 1;
-		
+
 	}
 	return (-1);
 }
